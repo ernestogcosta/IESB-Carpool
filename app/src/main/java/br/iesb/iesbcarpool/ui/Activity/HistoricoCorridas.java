893e42fec57d328352defa10892adfea4e654537
Ernesto Costa <ernestogcosta@gmail.com>
2019-06-10 13:40:20 -0300

    Recycler View Histórico Corridas - terminando adapter

---
+++ app/src/main/java/br/iesb/iesbcarpool/ui/Activity/HistoricoCorridas.java
@@ -0,0 +1,15 @@
+package br.iesb.iesbcarpool.ui.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.iesb.iesbcarpool.R;
+
+public class HistoricoCorridas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico_corridas);
+    }
+}
--- app/src/main/java/br/iesb/iesbcarpool/ui/recycleview/CorridaAdapter.java
+++ app/src/main/java/br/iesb/iesbcarpool/ui/recycleview/CorridaAdapter.java
@@ -1,46 +1,55 @@
 package br.iesb.iesbcarpool.ui.recycleview;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.iesb.iesbcarpool.R;
 import br.iesb.iesbcarpool.ui.Model.Corrida;
 import br.iesb.iesbcarpool.ui.Model.Usuario;
 
+import static android.support.constraint.Constraints.TAG;
+
 public class CorridaAdapter extends RecyclerView.Adapter<CorridaAdapter.ViewHolderCorrida> {
 
-    List<Corrida> dados;
+    private List<Corrida> dados = new ArrayList<>();
+    private Context mContext;
 
-    public CorridaAdapter(List<Corrida> dados) {
+    public CorridaAdapter(List<Corrida> dados, Context mContext) {
         this.dados = dados;
+        this.mContext = mContext;
     }
 
     @NonNull
     @Override
-    public CorridaAdapter.ViewHolderCorrida onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
-        View view = layoutInflater.inflate(R.layout.corrida_historico, viewGroup, false);
+    public CorridaAdapter.ViewHolderCorrida onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.from(parent.getContext()).inflate(R.layout.corrida_historico, parent, false);
 
-        ViewHolderCorrida holderCorrida = new ViewHolderCorrida(view);
+        ViewHolderCorrida holder = new ViewHolderCorrida(view);
 
-        return holderCorrida;
-
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CorridaAdapter.ViewHolderCorrida viewHolder, int i) {
+    public void onBindViewHolder(@NonNull CorridaAdapter.ViewHolderCorrida holder, int position) {
+        Log.d(TAG, "onBindViewHolder: chamado");
+
         if (dados != null && dados.size() > 0){
-            Corrida corrida = dados.get(i);
-            viewHolder.iesbDestino.setText(corrida.getIesb());
-            viewHolder.motorista.setText(corrida.getNomeMotorista());
-            viewHolder.horarioPartida.setText((String.valueOf(corrida.getHoraPartida())));
-            viewHolder.horarioChegada.setText((String.valueOf(corrida.getHoraDestino())));
+            Corrida corrida = dados.get(position);
+            holder.iesbDestino.setText(corrida.getIesb());
+            holder.motorista.setText(corrida.getNomeMotorista());
+            holder.horarioPartida.setText((String.valueOf(corrida.getHoraPartida())));
+            holder.horarioChegada.setText((String.valueOf(corrida.getHoraDestino())));
         }
     }
 
@@ -54,6 +63,7 @@ public class CorridaAdapter extends RecyclerView.Adapter<CorridaAdapter.ViewHold
         public TextView motorista;
         public TextView horarioPartida;
         public TextView horarioChegada;
+        ConstraintLayout historicoLayout;
 
         public ViewHolderCorrida(@NonNull View itemView) {
             super(itemView);
@@ -62,6 +72,8 @@ public class CorridaAdapter extends RecyclerView.Adapter<CorridaAdapter.ViewHold
             motorista = itemView.findViewById(R.id.idRecycleMotorista);
             horarioPartida = itemView.findViewById(R.id.idRecycleHorarioPartida);
             horarioChegada = itemView.findViewById(R.id.idRecycleHorarioChegada);
+            historicoLayout = itemView.findViewById(R.id.historico_layout);
         }
     }
+
 }
